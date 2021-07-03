828ed0d7dba5da0626df4d4e90db3bbb497f0fc7
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-03 13:59:47 +0200

    clean code

--- joker.js
+++ joker.js
@@ -13,8 +13,8 @@ window.addEventListener('click', () => {
     joker.setAttribute('id', 'joker');
     joker.addEventListener('click', () => {
         const inputText = document.querySelector("#main > footer > div.vR1LG._3wXwX.copyable-area > div._2A8P4 > div > div._2_1wd.copyable-text.selectable-text");
-        const request = new XMLHttpRequest();
 
+        const request = new XMLHttpRequest();
         request.open("GET", "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist,explicit&format=txt");
         request.addEventListener('load', () => {
             if (request.status >= 200 && request.status < 300) {
