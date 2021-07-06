d5ba83fdb4edf554b492cf3833ba016fb93fc3a4
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-06 20:00:51 +0200

    get more jokes

--- joker.js
+++ joker.js
@@ -17,7 +17,7 @@ window.addEventListener('click', () => {
         const inputText = document.querySelector("#main > footer > div.vR1LG._3wXwX.copyable-area > div._2A8P4 > div > div._2_1wd.copyable-text.selectable-text");
 
         const request = new XMLHttpRequest();
-        request.open("GET", "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political,racist,sexist,explicit&format=txt");
+        request.open("GET", "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist&format=txt");
         request.addEventListener('load', () => {
             if (request.status >= 200 && request.status < 300) {
                 inputText.innerHTML = request.responseText;
